1b738b8f9182e92186134a74ca6292116e7f3765
Bre 97 <67639369+bre97-web@users.noreply.github.com>
2021-05-06 17:32:03 +0800

    Add files via upload

---
+++ Code/Declaration.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#ifndef statement_H_INCLUDED
+#define statement_H_INCLUDED
+
+int AwwCalculator();
+void Convert(char* value);
+void SetValueResult(double value);
+double GetValueResult();
+void SetValue(char* value[128]);
+char* GetValue();
+
+
+#endif
--- Code/Main.c
+++ Code/Main.c
@@ -1,1 +1,68 @@
- 
+
+#include <stdio.h>
+#include <string.h>
+
+#include "Report.h"
+#include "Declaration.h"
+
+struct Value {
+  double result;
+  char o[128];
+}Set;
+
+
+
+
+int main(void) {
+
+
+
+
+  return REPORT_ACTIVE_FUNCTION_END;
+}
+
+int AwwCalculator() {
+  printf("Put in :");
+  
+  
+  
+
+  return REPORT_ACTIVE_FUNCTION_END;
+}
+
+void Convert(char* value) {
+  
+  int index = 0;
+  char
+  
+  for(index = strlen(GetValue());index > 0;index --) {
+    if(!GetValue()[index] > 0 && GetValue()[index] < z) {
+      
+    
+    }
+  
+  }
+  SetValueResult();
+
+} 
+
+void SetValueResult(double value) {
+  Set.result = value;
+}
+
+double GetValueResult() {
+  return Set.result;
+}
+
+void SetValue(char* value[128]) {
+  int index = 0;
+  for (index = strlen(Set.o);index > 0;index --) {
+    Set.o[index] = *value[index];
+  }
+  
+  
+}
+
+char* GetValue() {
+  return Set.o;
+}
