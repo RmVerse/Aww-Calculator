9a0d47ef771aafad4cfb890b813491e8bf5ecb97
Bre 97 <67639369+bre97-web@users.noreply.github.com>
2021-06-30 18:50:42 +0800

    Add files via upload

--- Code/Main.c
+++ Code/Main.c
@@ -1,209 +1,403 @@
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+
+
+
+#include <direct.h>
+#include <io.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <time.h>
 #include <windows.h>
-#include <process.h>
 
-#include "Platform.C.Rmdust.Description.h"
-#include "Declaration.h"
-#include "Report.h"
 
-struct UserSurfaceIndexValue {
-  char String[128];
-} UserSurface;
-struct InternalSurfaceIndexValue {
-  double Result;
-} InternalSurface;
+void Rmdust_System_Console_Color_Update();
+int Rmdust_Reset();
+int Rmdust_System_Log_GetLableLevel();
+char* Rmdust_System_Log_GetLableLevel_ToString();
+void Rmdust_System_Log_SetLogActive(bool Active);
+bool Rmdust_System_Log_GetLogActive();
+void Rmdust_System_Log_SetTitle(char* Message);
+char* Rmdust_System_Log_GetTitle();
+void Rmdust_System_Log_SetTitleAddons(char* Message);
+char* Rmdust_System_Log_GetTitleAddons();
+void Rmdust_System_Log_SetMessage(char* Message);
+char* Rmdust_System_Log_GetMessage();
+void Rmdust_System_Console_Graphic_Animation_BasicMessage(char* Message);
+void Rmdust_System_Console_Graphic_Animation_LeaveMessage(char* Message);
+void Rmdust_System_Console_Graphic_Animation_Load();
+inline void static __Print();
+inline void static __Println();
+inline void static __Printf();
+void Rmdust_System_Console_Graphic_Text_Window(char* Message);
+void Rmdust_System_Console_Graphic_Text_Window_SquareLine(char* Message);
+int Rmdust_System_Console_Graphic_Text_Logo();
+inline void static __SetPos(int x,int y);
+int Rmdust_System_IO_Error_Log_Reset();
+int Rmdust_System_IO_Error_Log_Write();
+int Rmdust_System_IO_Folder_Create(char* FolderName);
 
-int main(void) {
 
-  Rmdust_Reset();
-  
-  // 创建线程
-  CreateThread(0, 0, (LPTHREAD_START_ROUTINE)StartConsole, 0, 0, 0);
+// New
+#define AND &&
+#define OR ||
+#define and &&
+#define or ||
+#define Graphic_Animation_Transition_Time 25
 
-  AwwCalculator();
-  
-  //ExitThread((LPTHREAD_START_ROUTINE)StartConsole);
+static char __Text = '=';
+static char __Space = ' ';
+static unsigned short int __WindowsHeight = 40;
+static unsigned short int __WindowsWidth = 120;
 
-  return 0;
+static unsigned short int __WindowWidthCenter = 60;
+static unsigned short int __WindowWidthEnd = 57;
+
+
+struct System {
+  // Of Console ColorCode 
+  struct SystemConsole {
+    struct SystemConsoleColor {
+      enum SystemConsoleColorList {
+        Black,
+        Blue,
+        Green,
+        GrayBlue,
+        Red,
+        Purple,
+        GrayYellow,
+        GrayWhite,
+        GrayBlack,
+        LightBlue,
+        LightGreen,
+        LightGrayBlue,
+        LightRed,
+        LightPurple,
+        LightYellow,
+        White
+      } Background,Foreground;
+
+      // Console-Color need this
+      // Rmdust_System_Console_Color_Update() Use this
+
+
+    } Color;
+
+  } Console;
+
+  // Of Logs
+  struct SystemLog {
+    char* Title;
+    char* TitleAddons;
+    char* Message;
+
+    bool Active;
+
+    enum SystemLogLableList {
+      Error,
+      Warning,
+      Non 
+    } Lable;
+
+    int MessageIndex;
+
+  } Log;
+
+} System;
+
+
+// Use this Before : Change struct -> System.Console.Color.Background
+//                                    System.Console.Color.Foreground
+void Rmdust_System_Console_Color_Update() {
+  HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+
+  SetConsoleTextAttribute(handle,(WORD)System.Console.Color.Foreground + (WORD)System.Console.Color.Background * 0x10);
 }
 
-void StartConsole(void *a) { 
-  system(
-      "start C:\\Users\\YCJX\\Desktop\\bre97-web$AwwCalculator\\AwwCalculator\\Release\\SystemConsole.exe");
-  ExitThread(0);
+
+// Initialize
+// Should use this in first
+int Rmdust_Reset() {
+  Rmdust_System_IO_Folder_Create("C:\\Users\\Public\\Documents\\RMDUST");
+  
+  Rmdust_System_Log_SetLogActive(false);
+  System.Log.Lable = Non;
+  Rmdust_System_IO_Error_Log_Reset();
+
+  return 1;
 }
 
-int AwwCalculator() {
 
-  // User Surface
-  printf("Put in :");
-  SetUserSurfaceValue(gets_s(UserSurface.String, 128));
-  printf("%s \n %d", GetUserSurfaceValue(),(int)GetUserSurfaceValueStrlength());
-
-  // Internal Surface
-
-  if (!VerifyDataRational()) {
-    printf("合法性验证:错误");
-
-    return 0;
+// Lable
+/*
+ * 0 : Error
+ * 1 : Warning
+ * 2 : Non
+ */
+int Rmdust_System_Log_GetLableLevel() { 
+  return System.Log.Lable;
+}
+char* Rmdust_System_Log_GetLableLevel_ToString() {
+  switch (Rmdust_System_Log_GetLableLevel()) { 
+    case 0:
+      return "Error";
+      break;
+    case 1:
+      return "Warning";
+      break;
+    case 2:
+      return "Non";
+      break;
+    default:
+      return "null";
   }
-
-  // User Surface
-
-  return 1;
 }
 
-int VerifyDataRational() {
-  VerifyDataRational_Symbol();
 
-  return 1;
+// Write logs's Activity  status
+void Rmdust_System_Log_SetLogActive(bool Active) { 
+  System.Log.Active = Active;
+}
+bool Rmdust_System_Log_GetLogActive() { 
+  return System.Log.Active; 
 }
 
-int VerifyDataRational_Symbol() {
-  if (!(VerifyDataRational_Symbol_InternalSymbol() && VerifyDataRational_Symbol_Monomial())) {
-    return 0;
+// Logs Information
+/*
+ * -;Title-|
+ * TitleAddons
+ * Message
+ * -;
+ */
+void Rmdust_System_Log_SetTitle(char* Message) {
+  System.Log.Title = Message;
+}
+char* Rmdust_System_Log_GetTitle() { 
+  return System.Log.Title; 
+}
+void Rmdust_System_Log_SetTitleAddons(char* Message) {
+  System.Log.TitleAddons = Message;
+}
+char* Rmdust_System_Log_GetTitleAddons() {
+  return System.Log.TitleAddons;
+}
+void Rmdust_System_Log_SetMessage(char* Message) {
+  System.Log.Message = Message;
+}
+char* Rmdust_System_Log_GetMessage() {
+  return System.Log.Message;
+}
+
+
+// It printf to end 
+void Rmdust_System_Console_Graphic_Animation_BasicMessage(char* Message) {
+  size_t MessageLength = strlen(Message);
+
+  for (size_t Index;Index < MessageLength;Index += 1) {
+    Sleep(Graphic_Animation_Transition_Time);
+    printf("%c", *(Message + Index));
   }
-
-  return 1;
 }
 
-int VerifyDataRational_Symbol_InternalSymbol() {
+// It printf to end and leave screeen
+void Rmdust_System_Console_Graphic_Animation_LeaveMessage(char* Message) {
+  size_t MessageLength = strlen(Message);
   
-  for (int Index = GetUserSurfaceValueStrlength_ToIndex(); Index >= 0;Index--) {
-    if (!(VerifyDataRational_Symbol_InternalSymbolContinuous_Arithmetic_Basic(Index) ||
-          VerifyDataRational_Symbol_InternalsymbolContinuous_Arithmetic_Pro(Index) ||
-          VerifyDataRational_Symbol_InternalSymbolContinuous_Logic(Index) ||
-          VerifyDataRational_Symbol_InternalSymbolContinuous_Number(Index))) {
-      
+  system("cls");
+  Rmdust_System_Console_Graphic_Animation_BasicMessage(Message);
 
-      Rmdust_SetLogsWriteActive(true);
-      Rmdust_SetSystemMessage_Message_ToChar(GetUserSurfaceValue_ToIndex(Index));
+  // This Use 
+  char MessageIn[MessageLength];
+  strcpy(MessageIn,Message);
+  for (size_t Index = 0;Index < MessageLength;Index += 1) {
+    size_t MessageIndexTemp = Index;
 
 
+    system("cls");
+    for (size_t InternalIndex = 0;InternalIndex < MessageLength;InternalIndex += 1) {
+      if (MessageIn[Index] < MessageIn[InternalIndex]) {
+        MessageIndexTemp = InternalIndex;
+      }
     }
+
+    if (MessageIndexTemp != Index) {
+      MessageIn[MessageIndexTemp] = ' ';
+    }
+
+    printf("%s", MessageIn);
+    Sleep(Graphic_Animation_Transition_Time);
+  }
+}
+
+void Rmdust_System_Console_Graphic_Animation_Load() {
+  for (int Count = 5;Count > 0;Count--) {
+    __SetPos(60,20);
+    printf("|");
+    Sleep(100);
+
+    __SetPos(60,20);
+    printf("/");
+    Sleep(100);
+
+    __SetPos(60,20);
+    printf("-");
+    Sleep(200);
+
+    __SetPos(60,20);
+    printf("/");
+    Sleep(100);
+  }
+}
+
+
+inline void static __Print() {
+  for (unsigned short int Count = __WindowsWidth - 1;Count > 0 ;Count -= 1) {
+    printf("%s" , __Text);
+  }
+}
+inline void static __Println() {
+  __Print();
+  printf("\n");
+}
+inline void static __Printf() {
+  printf("\n");
+  __Print();
+}
+
+// Text Window
+void Rmdust_System_Console_Graphic_Text_Window(char* Message) {
+  __Println();
+  printf("%s" , Message);
+  __Printf();
+}
+
+// Text Window
+// Support English/Chinese/Number In Windows 7
+//         English/       /Number In Windows 10
+void Rmdust_System_Console_Graphic_Text_Window_SquareLine(char* Message) {
+  size_t MessageLength = strlen(Message);
+
+  __Println();
+  printf("%c" , __Text);
+  for (unsigned short int Count = __WindowWidthCenter - MessageLength/2;Count > 0;Count -= 1) {
+    printf("%c" , __Space);
   }
   
-  Rmdust_SetSystemMessage_LableLevel("Error");
-  Rmdust_SetSystemMessage_Title_ToString("Text");
-  Rmdust_SetSystemMessage_TitleAddons_ToString("From Function VerifyDataRational_Symbol_InternalSymbol()");
-
-  Rmdust_System_IO_Logs_Write();
-
-  return 1;
-}
-
-int VerifyDataRational_Symbol_ExternalSymbol(int index) {
-  char message = GetUserSurfaceValue()[index];
-
-  if (message == '=' || message == ':') {
-    return 1;
+  printf("%s" , Message);
+  for (unsigned short int Count = __WindowWidthEnd - 1 - (MessageLength % 2 == 0 ? MessageLength / 2 - 1 : MessageLength / 2);Count > 0 ;Count -= 1) {
+    printf("%c" , __Space);
   }
-
-  return 0;
+  printf("%c" , __Text);
+  __Printf();
 }
 
-inline int VerifyDataRational_Symbol_InternalSymbolContinuous_Logic(int Index) {
-  char Message = GetUserSurfaceValue()[Index];
-  if (Message == '&' || Message == '|' || Message == ' ' || Message == '\0') {
-    return 1;
-  }
-  return 0;
-}
-inline int VerifyDataRational_Symbol_InternalsymbolContinuous_Arithmetic_Pro(
-    int Index) {
-  char Message = GetUserSurfaceValue()[Index];
-  if (Message == '!' || Message == '*' || Message == '/' || Message == '%') {
-    return 1;
-  }
-  return 0;
-}
-inline int VerifyDataRational_Symbol_InternalSymbolContinuous_Arithmetic_Basic(
-    int Index) {
-  char Message = GetUserSurfaceValue()[Index];
-  if (Message == '+' || Message == '-') {
-    return 1;
-  }
-  return 0;
-}
-inline int VerifyDataRational_Symbol_InternalSymbolContinuous_Number(int Index) {
-  char Message = GetUserSurfaceValue()[Index];
-  if (Message >= '0' && Message <= '9') {
-    return 1;
-  }
-  return 0;
-}
+// I use mine when I Start Console
+// It read local file to Display to screen
+int Rmdust_System_Console_Graphic_Text_Logo() {
+  FILE* LocalPath = NULL;
+  fopen_s(&LocalPath,"Resource/Logo.txt","r");
 
-
-int VerifyDataRational_Symbol_Monomial() {
-  if (!VerifyDataRational_Symbol_Monomial_FindInternalSymbol()) {
-
+  if (LocalPath == NULL) {
     return 0;
   }
 
+  while(!feof(LocalPath)){
+    printf("%c",(char)fgetc(LocalPath));
+  }
+  
   return 1;
 }
 
-// 寻找内部符号
-int VerifyDataRational_Symbol_Monomial_FindInternalSymbol() {
-  if (VerifyDataPath_Monomial_FindInternalSymbol_FindStarIndexIsNumber() && VerifyDataPath_Monomial_FindInternalSymbol_FindNumber()) {
-    return 1;
+// Console pos direction
+inline void static __SetPos(int x,int y) {
+  COORD point = {
+    x , y 
+  };
+    
+  HANDLE HOutput = GetStdHandle( STD_OUTPUT_HANDLE );
+  SetConsoleCursorPosition(HOutput, point);
+}
+
+
+// Use this when I start program
+int Rmdust_System_IO_Error_Log_Reset() {
+  FILE* FilePath = NULL;
+  fopen_s(&FilePath, "C:\\Users\\Public\\Documents\\RMDUST\\Log.txt", "w+");
+
+  if (FilePath == NULL) {
+    return -1;
   }
 
-  return 0;
+  fclose(FilePath);
+
+  return 1;
 }
-// 一句表达式必须包含至少一个内部数字
-inline int VerifyDataPath_Monomial_FindInternalSymbol_FindNumber() {
-  for (int Index = GetUserSurfaceValueStrlength_ToIndex(); Index >= 0;
-       Index--) {
-    if (VerifyDataRational_Symbol_InternalSymbolContinuous_Number(Index)) {
-      return 1;
-    }
+
+
+int Rmdust_System_IO_Error_Log_Write() {
+  if (!Rmdust_System_Log_GetLogActive()) {
+    return 0;
   }
 
-  return 0;
-}
-// 起始符号必须是数字或是定义符号
-inline int VerifyDataPath_Monomial_FindInternalSymbol_FindStarIndexIsNumber() {
-  if (VerifyDataRational_Symbol_InternalSymbolContinuous_Number(0)) {
-    return 1;
+  FILE* FilePath = NULL;
+  fopen_s(&FilePath, "C:\\Users\\Public\\Documents\\RMDUST\\Log.txt", "a+");
+
+  if (FilePath == NULL) {
+    return 0;
   }
-  return 0;
+
+  fprintf(FilePath, "-;\n-%d%s\n-5%s\n%s\n%s\n-;\n\n",
+          Rmdust_System_Log_GetLableLevel(),
+          Rmdust_System_Log_GetLableLevel_ToString(),
+          Rmdust_System_Log_GetTitle(),
+          Rmdust_System_Log_GetTitleAddons(),
+          Rmdust_System_Log_GetMessage());
+
+  fclose(FilePath);
+
+  Rmdust_System_Log_SetLogActive(false);
+
+  return 1;
 }
+
+
+int Rmdust_System_IO_Folder_Create(char* FolderName) {
+  if (!_access(FolderName, 0)) {
+    return 0;
+  }
+
+  return _mkdir(FolderName);
+}
+
+
 
 /*
-// 路径分析 :
-int VerifyDataPath() { VerifyDataPath_VerifyMultinomial(); }
-
-// 路径分析分支 : 验证路径能否被分组为多项式
-int VerifyDataPath_VerifyMultinomial() { VerifyDataPath();
-  // 寻找所有的计算符号坐标
-  
-
+int _System_Time_GetDay() {
+  time_t systemTime;
+  struct tm *systemTimeAddress;
+  time(&systemTime);
+  gmtime_s(&systemTime, systemTimeAddress);
+  return systemTimeAddress->tm_mday;
 }
-
-inline int VerifyDataPath_() {
-  
-
-
+int _System_Time_GetMonth() {
+  time_t systemTime;
+  struct tm *systemTimeAddress;
+  time(&systemTime);
+  gmtime_s(&systemTime, systemTimeAddress);
+  return systemTimeAddress->tm_mon;
 }
-
 */
 
-size_t GetUserSurfaceValueStrlength() { return strlen(UserSurface.String); }
-unsigned int GetUserSurfaceValueStrlength_ToIndex() {
-  return (unsigned int)(GetUserSurfaceValueStrlength() == 0
-                            ? 0
-                            : GetUserSurfaceValueStrlength() - 1);
-}
-
-void SetUserSurfaceValue(char* Message) { *UserSurface.String = *Message; }
-char* GetUserSurfaceValue() { return UserSurface.String; }
-
-char GetUserSurfaceValue_ToIndex(int Index) {
-  //return GetUserSurfaceValue()[Index];
-  return UserSurface.String[Index];
-}
+/*
+ *  Writer : Bre 97
+ *    Writer Information :
+ *      Twitter-ID :     @Bre97_web
+ *      Google-Mail :    BY.SGATN3@Gmail.com
+ *      Outlook-Mail :   BY_SGATN3@Outlook.com
+ *      GitHub-Name :    Bre97-web
+ *    Writer Words :
+ *      My Information is above this.
+ *      You can give me with "Error Report"as I will Carefully.
+ *      You can Upgrade this File,but you should have told me.
+ *  About :
+ *    Last Update date : 6.28.2021
+ */
