1e68d4385d280c3a4187e8438669dfa8a7a22c74
Bre 97 <67639369+bre97-web@users.noreply.github.com>
2022-04-01 10:04:23 +0800

    test

--- Code/Main.c
+++ Code/Main.c
@@ -1,97 +1,54 @@
 
-#include "Descript.h"
-#include "ShortcutKey.h"
-
 #include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
 #include <string.h>
-#include <conio.h>
+#include <stdbool.h>
 
 
 
+#define and &
+#define or ||
+#define in ==
+#define is ==
 
 
+bool In(char s, char String[]) {
+	
+	for(size_t index = 0; index < strlen(String); index ++) if(s == String[index]) return true;
+	
+	return false;
+}
 
 
+char* ReadLine(char String[]) {
+	
+	scanf("%s", String);
+	
+	return String;
+}
+
+char* Format(char String[]) {
+
+	for(size_t index = 0; index < strlen(String); index ++) {
+		
+		if(String[index] >= '0' and String[index] <= '9') continue;
+		if(In(String[index],"+-*/")) continue;
+		
+		String[index] = '\x20';
+	}
 
 
-
-
-
-
-
-
-
-
-
+	return String;
+}
 
 
 int main(void) {
 
-        Putin();
-
-
-
-
-        return 0;
-
-}
-
-
-void Putin() {
-
-        char List[256 + 1];
-
-        struct _PutIn {
-                int HeadCode;
-                int MainCode;
-        } PutIn;
-
-        for(size_t Index = 0; Index < 256; Index ++) {
-
-                
-
-                PutIn.HeadCode = _getch();
-                PutIn.MainCode = _getch();
-
-                
-                switch (PutIn.HeadCode) {
-                        case Key_HeadCode_IndependentControl :
-                                goto IndependentControl;
-                                break;
-
-                        default :
-                                goto Others;
-                                break; 
-                                
-                }
-
-                IndependentControl :
-                if(PutIn.MainCode == Key_Esc) {
-                        exit(0);
-                }
-
-                Others:
-                List[Index] = PutIn.MainCode;
-
-                system("cls");
-
-                printf("%s\n", List);
-                for(size_t i = 0; i < strlen(List); i ++) {
-                        printf("%c", '\x20');
-
-                }
-                printf("%c", '`');
-
-                
-        }
-
-
-}
-
-
-void PutPosition() {
-
-
+	char Formula[512] = "";
+	
+	strcpy(Formula, ReadLine(Formula));
+	
+	strcpy(Formula, Format(Formula));
+	printf("%s", Formula);
+	
+	return 0;
 }
